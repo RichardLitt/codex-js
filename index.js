dd9eb5d499573f8e7f07b2a1d5dcd37b3c5e2e6c
Richard Littauer <richard.littauer@gmail.com>
2014-11-30 13:47:59 -0500

    Added in a few more functions with comments
    These need to be added to the cli, or the cli needs to be abstracted
    out into a more easy-to-use program so that all of this code isn't
    being repeated. Ideally, Karma (#1) would be instituted and we could
    verify each function as we go through it. There ought to be a visual
    interface as well.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // From: 
 // http://blog.izs.me/post/2353458699/an-open-letter-to-javascript-leaders-regarding
-var incrementOverNewLine = function(i, j, cb) {
+// In general, \n ends a statement unless:
+var semicolons = {}
+
+// The statement has an unclosed paren, array literal, or object literal or ends in some other way that is not a valid way to end a statement. (For instance, ending with . or ,.)
+semicolons.unclosedLiteral = function() {}
+
+semicolons.incrementOverNewLine = function(i, j, cb) {
 
   if ((i || j) == null) return cb(new Error('No iterators provided!')) 
 
@@ -10,6 +16,34 @@ var incrementOverNewLine = function(i, j, cb) {
   j
 
   return cb(null, i, j) 
+}
+
+// It is a for(), while(), do, if(), or else, and there is no {
+semicolons.noOpeningBracket = function() {}
+
+//; is a valid JavaScript statement, so if(x); is equivalent to if(x){} or, “If x, do nothing.” 
+semicolons.ifDoNothing = function() {
+
+  if(i);
+  // No error
+}
+
+// That is, if you have a \n immediately after a return, throw, break, or continue token, or a ++ or -- as a postfix operator (that is, x++\n or y--\n), then it will terminate the statement, no exceptions.
+semicolons.returnBreak = function(i, cb) {
+  return i
+      ++ // Will not increment
+}
+
+semicolons.infiniteLoop = function(i, cb) {
+  // This will go on forever
+  while(i);
+}
+
+semicolons.infiniteSemicolons = function(i, cb) {
+  ;;;i++;;;
+
+  // This will be i++, as all the statements above are valid
+  return i
 }
 
 
